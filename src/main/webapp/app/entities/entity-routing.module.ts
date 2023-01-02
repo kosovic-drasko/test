a79413e7acf8b2945265b1ee291b409b47a72258
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2023-01-02 05:41:05 +0100

    k

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'ponude',
+        data: { pageTitle: 'testApp.ponude.home.title' },
+        loadChildren: () => import('./ponude/ponude.module').then(m => m.PonudeModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
